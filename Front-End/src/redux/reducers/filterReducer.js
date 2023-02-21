52cc67bf297120d55361082f4aec9622369ab0d7
Office PC ABA (Fahad) <abasc1962@gmail.com>
2023-02-21 15:14:40 +0600

    Filters Added in the Filters Array of the State

--- Front-End/src/redux/reducers/filterReducer.js
+++ Front-End/src/redux/reducers/filterReducer.js
@@ -1,7 +1,34 @@
+import { TOGGLE_BRAND, TOGGLE_STOCK } from "../actionTypes/actionTypes";
+
 export const initialState = {
-    test: "TEST"
+    filters: {
+        brands: [],
+        stock: false,
+    },
+    keyword: ""
 };
 
 export const filterReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case TOGGLE_BRAND:
+            if (!state.filters.brands.includes(action.payload)) {
+                return {
+                    ...state,
+                    filters: { ...state.filters, brands: [...state.filters.brands, action.payload] }
+                }
+            } else {
+                return {
+                    ...state,
+                    filters: { ...state.filters, brands: state.filters.brands.filter(brand => brand !== action.payload) }
+                }
+            }
+
+        case TOGGLE_STOCK:
+            return {
+
+            }
+
+        default:
+            return state;
+    }
 }
